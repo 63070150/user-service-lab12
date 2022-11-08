e2fde83ffe1993cf7b150e2dfd68a8f636d83a69
Rammakorn <63070150@it.kmitl.ac.th>
2022-11-08 22:22:38 +0700

    done

---
+++ src/main/java/com/sop/chapter10/userservicelab12/UserServiceLab12Application.java
@@ -0,0 +1,13 @@
+package com.sop.chapter10.userservicelab12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceLab12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceLab12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sop/chapter10/userservicelab12/UserServiceLab12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.chapter10.userservicelab12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceLab12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
